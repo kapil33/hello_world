4e9bc1360f6ffd7fb50011c2be7f8c67d1e02b21
Kapil Test <kapiltest@kapils-MacBook-Pro.local>
2024-10-06 00:17:44 +0530

    improving TC for kTopPerformer operation

--- src/main/java/interviews/twentytwentyfour/truefoundry/ReviewDataStore.java
+++ src/main/java/interviews/twentytwentyfour/truefoundry/ReviewDataStore.java
@@ -16,28 +16,47 @@ public class ReviewDataStore {
         this.employeeIdToReviewMap = new HashMap<>();
         this.reviewerIdToReviewMap = new HashMap<>();
         this.employeeIdToRatingSumMap = new HashMap<>();
-        pq = new PriorityQueue();
+        pq = new PriorityQueue<>((a,b) -> Double.compare(b.getValue(), a.getValue()));
     }
 
+    // TC: O(1)
     public void insert(Review review) {
+        // create Pair<employeeId, avgRating>
+        Pair<Integer, Double> pair = new Pair<>(review.employeeId, getAvgRatingByEmployeeId(review.employeeId));
+        // then remove it from the pq
+        pq.remove(pair);
+
         employeeIdToReviewMap.computeIfAbsent(review.employeeId, k->new ArrayList<>()).add(review);
         reviewerIdToReviewMap.computeIfAbsent(review.reviewerId, k->new ArrayList<>()).add(review);
+
+        // then add new sum of rating to employeeIdToRatingSumMap
         employeeIdToRatingSumMap.put(
-                review.employeeId, employeeIdToRatingSumMap.getOrDefault(review.employeeId, 0.0) + review.rating);
+                review.employeeId, employeeIdToRatingSumMap.getOrDefault(review.employeeId, 0d) + review.rating);
+        // create new Pair<employeeId, avgRating> and insert in PQ
+        pq.add(new Pair<>(review.employeeId, getAvgRatingByEmployeeId(review.employeeId)));
     }
 
+    // TC: O(1)
     public List<Review> getReviewsByEmployeeId(int employeeId) {
         return employeeIdToReviewMap.get(employeeId);
     }
 
+    // TC: O(1)
     public List<Review> getReviewsByReviewerId(int reviewerId) {
         return reviewerIdToReviewMap.get(reviewerId);
     }
 
+    // TC: O(1)
     public double getAvgRatingByEmployeeId(int employeeId) {
-        return employeeIdToRatingSumMap.get(employeeId)/employeeIdToReviewMap.get(employeeId).size();
+        Double ratingSum = employeeIdToRatingSumMap.get(employeeId);
+
+        if (ratingSum == null)
+            return 0;
+
+        return ratingSum/employeeIdToReviewMap.get(employeeId).size();
     }
 
+    // TC: O(n), n = total no. of reviews
     public List<Review> getReviewsInDateRange(String start, String end) {
         LocalDate startDate = DateUtility.getLocalDate(start);
         LocalDate endDate = DateUtility.getLocalDate(end);
@@ -54,7 +73,8 @@ public class ReviewDataStore {
         return result;
     }
 
-    public List<Pair<Integer, Double>> getTopPerformers(int n) {
+    // TC: O(nlogn), n = no. of employees, it takes O(nlogn) time to the build PQ and O(klogn) time to retrieve top k performers
+    /*public List<Pair<Integer, Double>> getTopPerformers(int k) {
         pq = new PriorityQueue<>((a,b) -> Double.compare(b.getValue(), a.getValue()));
         List<Pair<Integer, Double>> result = new ArrayList<>();
 
@@ -62,9 +82,22 @@ public class ReviewDataStore {
             pq.add(new Pair<>(employeeId, getAvgRatingByEmployeeId(employeeId)));
         }
 
-        while (n-- > 0) {
+        while (k-- > 0) {
             result.add(pq.poll());
         }
+
+        return result;
+    }*/
+
+    // TC: O(klogn), n = no. of employees, k = top k performers, it takes O(klogn) time to retrieve top k performers
+    public List<Pair<Integer, Double>> getTopPerformers(int k) {
+        List<Pair<Integer, Double>> result = new ArrayList<>();
+
+        while (k-- > 0) {
+            result.add(pq.poll());
+        }
+
+        pq.addAll(result);
 
         return result;
     }
