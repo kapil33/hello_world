c0568e8a2a33379ad90da533c4b94f796d198c25
Kapil Test <kapiltest@kapils-MacBook-Pro.local>
2024-12-16 01:50:58 +0530

    changed kadanes multiply

--- src/main/java/gfg/KadanesMultiply.java
+++ src/main/java/gfg/KadanesMultiply.java
@@ -3,15 +3,18 @@ package gfg;
 public class KadanesMultiply {
 
     private int mult(int[] nums){
-        int max = Integer.MIN_VALUE, max_so_far = 1;
+        int max = nums[0], currMax = nums[0], currMin = nums[0];
 
-        for(int i=0; i<nums.length; i++){
-            max_so_far *= nums[i];
+        for(int i=1; i<nums.length; i++){
+            if (nums[i] < 0) {
+                int temp = currMax;
+                currMax = currMin;
+                currMin = temp;
+            }
 
-            if (max < max_so_far)
-                max = max_so_far;
-            if (max_so_far <= 0)
-                max_so_far = 1;
+            currMax = Math.max(nums[i], currMax*nums[i]);
+            currMin = Math.min(nums[i], currMin*nums[i]);
+            max = Math.max(max, currMax);
         }
 
         return max;
@@ -23,7 +26,7 @@ public class KadanesMultiply {
         int[] nums1 = new int[]{-2, -3, 4, -1, -2, 1, 5, -3};
         int[] nums2 = new int[]{-3, -1, -2, -3, -5};
 
-        int ans = kdm.mult(nums1);
-        System.out.println("Largest multiply contiguous array is: " + ans);
+        System.out.println("Largest multiply contiguous array is: " + kdm.mult(nums1));
+        System.out.println("Largest multiply contiguous array is: " + kdm.mult(nums2));
     }
 }
